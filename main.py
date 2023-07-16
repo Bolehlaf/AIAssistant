5c1472a74fd088bf3d067e5e6d8d5b1e3197ce71
Václav Štula <vaclav@stulovi.cz>
2023-07-16 21:47:02 +0200

    default implementation

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import speech_recognition as sr
+import openai
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Set up your OpenAI API credentials
+openai.api_key = 'YOUR_API_KEY'
 
+# Function to recognize speech from audio
+def recognize_speech():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        return "Sorry, I couldn't understand."
+    except sr.RequestError:
+        return "Sorry, I couldn't access the speech recognition service."
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Function to interact with OpenAI API
+def ask_openai(question):
+    response = openai.Completion.create(
+        engine='davinci',
+        prompt=question,
+        max_tokens=50,
+        temperature=0.7,
+        n=1,
+        stop=None,
+        timeout=10
+    )
+    return response.choices[0].text.strip()
 
+# Main loop for the personal assistant
+def main():
+    print("Personal Assistant is ready.")
+    while True:
+        user_input = recognize_speech()
+        print("You: ", user_input)
+        if user_input.lower() == 'exit':
+            break
+        response = ask_openai(user_input)
+        print("Assistant: ", response)
 
-# Press the green button in the gutter to run the script.
+# Run the personal assistant
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
