f9285be6f413edb0a5f96f55fd58ad98416dc182
Václav Štula <vaclav@stulovi.cz>
2023-07-20 20:36:02 +0200

    remove speech recognition, add gitignore

--- main.py
+++ main.py
@@ -1,47 +1,38 @@
-import speech_recognition as sr
+import os
 import openai
+from dotenv import load_dotenv
 
-# Set up your OpenAI API credentials
-openai.api_key = 'YOUR_API_KEY'
+# Load the variables from .env file
+load_dotenv()
 
-# Function to recognize speech from audio
-def recognize_speech():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        return text
-    except sr.UnknownValueError:
-        return "Sorry, I couldn't understand."
-    except sr.RequestError:
-        return "Sorry, I couldn't access the speech recognition service."
+# Set up your API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Function to interact with OpenAI API
-def ask_openai(question):
+
+def ask_chat_gpt(prompt):
     response = openai.Completion.create(
-        engine='davinci',
-        prompt=question,
-        max_tokens=50,
+        engine="text-davinci-002",  # GPT-3.5 engine
+        prompt=prompt,
         temperature=0.7,
+        max_tokens=150,
         n=1,
         stop=None,
-        timeout=10
+        echo=True
     )
     return response.choices[0].text.strip()
 
-# Main loop for the personal assistant
-def main():
-    print("Personal Assistant is ready.")
-    while True:
-        user_input = recognize_speech()
-        print("You: ", user_input)
-        if user_input.lower() == 'exit':
-            break
-        response = ask_openai(user_input)
-        print("Assistant: ", response)
 
-# Run the personal assistant
-if __name__ == '__main__':
+def main():
+    print("Hello! I'm your personal assistant. How can I help you today?")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit", "bye"]:
+            print("Goodbye!")
+            break
+        response = ask_chat_gpt(user_input)
+        print("Assistant:", response)
+
+
+if __name__ == "__main__":
     main()
+
